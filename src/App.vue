b33f30a1a0b8d6695bf1e2ef1895f4b458edd3ec
k1nz <kinz.qiu@foxmail.com>
2022-07-27 17:21:21 +0800

    wip: add line

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div style="display: flex; flex-direction: column; gap: 10px">
-    <button type="button" @click="">Add Line</button>
+    <button type="button" @click="handleAnchorAdd">Add Line</button>
     <div style="display: flex; background: aliceblue">
       <v-stage
         ref="stage"
         :config="{
-          width: 600,
-          height: 600,
+          width: 1200,
+          height: 700,
         }"
+        @click="handleStageClick"
+        @mousemove="handleStageMouseMove"
       >
         <v-layer
           ref="layer"
@@ -16,16 +18,19 @@
         >
           <v-line
             v-for="(linePoints, index) in lines"
-            :key="index"
+            :key="`line-${index}`"
             :config="{
               points: linePoints,
-              stroke: '#666',
-              strokeWidth: 2,
+              stroke: linesConfig[index].color,
+              strokeWidth: linesConfig[index].width,
+              lineCap: 'round',
+              lineJoin: 'round',
+              listening: !addMode,
             }"
-            @click="handleLineClick($event, linePoints, index)"
+            @click="handleLineClick($event, index)"
           />
           <v-circle
-            v-for="(anchor, idx) in activeAnchors"
+            v-for="(anchor, idx) in activeAnchorsGroup"
             :key="`anchor-${idx}`"
             :config="{
               x: anchor.x,
@@ -35,10 +40,14 @@
               fill: '#ddd',
               strokeWidth: 1,
               draggable: true,
+              listening:
+                !addMode || (!addMode && idx !== activeAnchorsGroup.length - 1),
             }"
             @dragmove="handleAnchorDrag($event, anchor, idx)"
+            @click="handleAnchorClick"
           />
           <v-circle
+            v-if="!addMode"
             v-for="(anchor, idx) in midAnchors"
             :key="`midAnchor-${idx}`"
             :config="{
@@ -50,7 +59,7 @@
               strokeWidth: 1,
               draggable: false,
             }"
-            @click="handleMiddleDragStart($event, anchor)"
+            @mousedown="handleMiddleDragStart($event, anchor)"
           />
         </v-layer>
       </v-stage>
@@ -59,12 +68,14 @@
 </template>
 
 <script>
-import { computed, ref, watch } from "vue";
+import { computed, ref, watch, nextTick } from "vue";
+import Konva from "konva";
 
 export default {
   setup() {
-    const activeIndex = ref();
+    const activeIndex = ref(-1);
 
+    /** @type Ref<Array<Array<{x: number, y: number}>> */
     const anchors = ref([
       [
         { x: 100, y: 100 },
@@ -86,16 +97,16 @@ export default {
       ],
     ]);
 
-    const activeAnchors = computed(() => {
+    const activeAnchorsGroup = computed(() => {
       return anchors.value[activeIndex.value] || [];
     });
 
     const midAnchors = computed(() => {
-      return activeAnchors.value
+      return activeAnchorsGroup.value
         .map((anchor, idx) => {
           if (idx === 0) return;
 
-          const prevAnchor = activeAnchors.value[idx - 1];
+          const prevAnchor = activeAnchorsGroup.value[idx - 1];
           const x = (prevAnchor.x + anchor.x) / 2;
           const y = (prevAnchor.y + anchor.y) / 2;
           const prev = prevAnchor;
@@ -109,14 +120,47 @@ export default {
         })
         .slice(1);
     });
+
     const handleAnchorDrag = (e, anchor, index) => {
-      activeAnchors.value[index] = e.target.getAbsolutePosition();
+      activeAnchorsGroup.value[index] = e.target.getAbsolutePosition();
     };
     const handleMiddleDragStart = (e, item) => {
-      const idx = activeAnchors.value.findIndex(
+      const idx = activeAnchorsGroup.value.findIndex(
         (anchor) => anchor === item.next
       );
-      activeAnchors.value.splice(idx, 0, item);
+      activeAnchorsGroup.value.splice(idx, 0, item);
+    };
+
+    // add line
+    const addMode = ref(false);
+    const handleAnchorAdd = async () => {
+      addMode.value = true;
+      anchors.value.push([{ x: 0, y: 0 }]);
+      linesConfig.value.push(DEFAULT_LINE_CONFIG);
+      await nextTick();
+      activeIndex.value = anchors.value.length - 1;
+    };
+    const updateAnchor = (anchor, pos) => {
+      anchor.x = pos.x;
+      anchor.y = pos.y;
+    };
+    const handleStageMouseMove = (e) => {
+      if (!addMode.value || e.target.tagName === "CANVAS") return;
+      const pos = e.target.getStage().getPointerPosition();
+      activeAnchorsGroup.value[activeAnchorsGroup.value.length - 1] = pos;
+      // updateAnchor(activeAnchorsGroup.value[activeAnchorsGroup.value.length - 1], pos)
+    };
+    const handleStageClick = (e) => {
+      if (!addMode.value) return;
+      if (e.target instanceof Konva.Line || e.target instanceof Konva.Circle)
+        return;
+      // console.log(e.target.getRelativePointerPosition())
+      activeAnchorsGroup.value.push(e.target.getStage().getPointerPosition());
+    };
+    // complete line
+    const handleAnchorClick = (e) => {
+      if (!addMode.value) return;
+      addMode.value = false;
     };
 
     const lines = computed(() => {
@@ -124,31 +168,64 @@ export default {
         ans.map((anchor) => [anchor.x, anchor.y]).flat()
       );
     });
-    const handleLineClick = (e, linePoints, index) => {
-      activeIndex.value === index
-        ? (activeIndex.value = undefined)
-        : (activeIndex.value = index);
+    const handleLineClick = (e, index) => {
+      if (activeIndex.value === index) {
+        activeIndex.value = -1;
+      } else {
+        activeIndex.value = index;
+      }
+    };
+    // 存放线与锚点不相关的数据
+    const DEFAULT_LINE_CONFIG = {
+      color: "#000",
+      width: 2,
+    };
+    const linesConfig = ref([
+      {
+        color: "#666",
+        width: 2,
+      },
+      {
+        color: "#666",
+        width: 2,
+      },
+      {
+        color: "#666",
+        width: 2,
+      },
+    ]);
+
+    const getLines = () => {
+      return lines.value.map((line, index) => ({
+        points: line,
+        color: linesConfig.value[index].color,
+        width: linesConfig.value[index].width,
+      }));
     };
 
     const handleMouseover = (e) => {
       document.body.style.cursor = "pointer";
     };
-    //mouseout
     const handleDragend = (e) => {
       document.body.style.cursor = "default";
     };
 
     return {
+      activeIndex,
       midAnchors,
       anchors,
       lines,
-      activeAnchors,
+      activeAnchorsGroup,
+      linesConfig,
+      addMode,
       handleMiddleDragStart,
       handleAnchorDrag,
       handleMouseover,
       handleDragend,
-      activeIndex,
       handleLineClick,
+      handleStageClick,
+      handleStageMouseMove,
+      handleAnchorAdd,
     };
   },
 };
